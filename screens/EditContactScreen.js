9d928414e15b0b6c16fee001ada41674c097f2a1
Victor Tram <victortram1996@gmail.com>
2019-09-20 16:12:21 -0700

    Edit Screen created

--- screens/EditContactScreen.js
+++ screens/EditContactScreen.js
@@ -1,12 +1,152 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, TouchableWithoutFeedback, Keyboard, AsyncStorage, Alert } from 'react-native';
+import {Form, Item, Input, Label, Button} from 'native-base';
+
 
 export default class EditContactScreen extends React.Component{
+
+    state = {
+        fname: "",
+        lname:"",
+        phone:"",
+        email:"",
+        address:"",
+        key:"",
+    }
+
+    componentDidMount(){
+        const {navigation} = this.props;
+        navigation.addListener("willFocus", () => {
+            var key = this.props.navigation.getParam("key","");
+            
+            // Get Contact Ingo
+            this.getContact(key);
+            
+        })
+    }
+
+    getContact = async(key) => {
+        await AsyncStorage.getItem(key)
+        .then( (contactJsonString) =>{
+            var contact = JSON.parse(contactJsonString);
+            contact["key"] = key;
+            this.setState(contact);
+        }
+
+        )
+        .catch(error =>{
+            console.log(error);
+        })
+    }
+
+    updateContact = async(key) => {
+        if(
+            this.state.fname !== "" &&
+            this.state.lname !== "" &&
+            this.state.phone !== "" &&
+            this.state.email !== "" &&
+            this.state.address !== "" 
+        ){
+            var contact = {
+                fname: this.state.fname,
+                lname: this.state.lname,
+                phone: this.state.phone,
+                email: this.state.email,
+                address: this.state.address,
+            }
+            await AsyncStorage.mergeItem(key, JSON.stringify(contact))
+            .then( () => {
+                this.props.navigation.goBack();
+            })
+            .catch(error => { console.log(error)})
+        }
+    }
+
+    static navigationOptions = {
+        title: "Edit Contact"
+    };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>EditContact Screen</Text>
-      </View>
+        <TouchableWithoutFeedback
+        onPress = { () => {
+            Keyboard.dismiss();
+        }}>
+            <ScrollView style={styles.container}>
+                <Form>
+                    <Item style={styles.inputItem}>
+                        <Label>First Name</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            keyboardType="default"
+                            onChangeText={fname => this.setState({fname})}
+                            value = {
+                                this.state.fname
+                            }
+                        />
+                    </Item>
+                    <Item style={styles.inputItem}>
+                        <Label>Last Name</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            keyboardType="default"
+                            onChangeText={lname => this.setState({lname})}
+                            value = {
+                                this.state.lname
+                            }
+                        />
+                    </Item>
+                    <Item style={styles.inputItem}>
+                        <Label>Phone</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            keyboardType="decimal-pad"
+                            onChangeText={phone => this.setState({phone})}
+                            value = {
+                                this.state.phone
+                            }
+                        />
+                    </Item>
+                    <Item style={styles.inputItem}>
+                        <Label>Email</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            keyboardType="default"
+                            onChangeText={email => this.setState({email})}
+                            value = {
+                                this.state.email
+                            }
+                        />
+                    </Item>
+                    <Item style={styles.inputItem}>
+                        <Label>Address</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            keyboardType="default"
+                            onChangeText={address => this.setState({address})}
+                            value = {
+                                this.state.address
+                            }
+                        />
+                    </Item>
+                </Form>
+                <Button
+                full rounded style={styles.button}
+                onPress = { () => {
+                    this.updateContact(this.state.key);
+                }}
+                >
+                    <Text style={styles.buttonText}>Update</Text>
+                </Button>
+                <View style={styles.empty}></View>
+            </ScrollView>
+        </TouchableWithoutFeedback>
+      
     );
   }
 }
@@ -27,5 +167,9 @@ const styles = StyleSheet.create({
     buttonText: {
       color: "#fff",
       fontWeight: "bold"
-    }
+    },
+    empty: {
+        height: 500,
+        backgroundColor: "#FFF"
+      }
   });
